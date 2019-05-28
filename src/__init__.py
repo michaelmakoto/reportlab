2e3e38a23a3de6a83c71e57b76689790b24bca94
michaelmakoto <michael.tut.global@gmail.com>
2019-05-28 12:37:09 +0900

    small updates

--- src/__init__.py
+++ src/__init__.py
@@ -3,9 +3,14 @@ from reportlab.pdfgen import canvas
 from reportlab.lib.units import mm
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.cidfonts import UnicodeCIDFont
+from reportlab.pdfbase.pdfmetrics import registerFont
+from reportlab.pdfbase.ttfonts import TTFont
+
 from bs4 import BeautifulSoup
 import requests
 import re
 import sys,os
 import itertools
 from PIL import Image
+
+from src.card import Generate_card
