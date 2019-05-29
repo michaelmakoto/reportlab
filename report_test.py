6318bf64049482ec36cf37ccc4c369b74010d00d
michaelmartinsen <michaelmartinsen@michaelmartinsen-no-Mac-mini.local>
2019-05-30 01:53:34 +0900

    minor changes

--- report_test.py
+++ report_test.py
@@ -1,14 +1,18 @@
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 from reportlab.lib.units import mm
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.cidfonts import UnicodeCIDFont
+from reportlab.pdfbase.pdfmetrics import registerFont
+from reportlab.pdfbase.ttfonts import TTFont
+
+from reportlab.lib.colors import Color,blue,white
+
 from bs4 import BeautifulSoup
 import requests
 import re
 import sys,os
-from reportlab.pdfbase import pdfmetrics
-from reportlab.pdfbase.cidfonts import UnicodeCIDFont
 import itertools
-
 from PIL import Image
 
 # card size
@@ -23,14 +27,9 @@ margin_y = 7
 gap_x = 4
 gap_y = 2
 
-#list of words
-listOfWrods = [
-    'sweltering','outgoing','flap','icy','ugliest',
-    'unknown','crowded','attack','ink','apple',
-    'participate','alien'
-    ]
+#card color
 
-#soup object
+r = 2.5
 
 # ---- main thread from here ----
 print('start...')
@@ -38,16 +37,34 @@ canvas = canvas.Canvas("form.pdf")
 canvas.setLineWidth(.1)
 pdfmetrics.registerFont(UnicodeCIDFont('HeiseiMin-W3'))
 
-filename = './img/card.png'
-im = Image.open(filename)
-width, height = im.size
+canvas.rect(0,0,card_width *mm, card_height *mm)
+my_green = Color(133/255,202/255,171/255)
+my_white = Color(1,1,1)
 
-print(width, height)
+# backgroug
+canvas.setFillColor(my_green)
+canvas.rect(0,0,card_width *mm,card_height *mm,fill=True,stroke=False)
 
-canvas.drawImage(filename,(12 + 0)*mm,(7 + 0)*mm)
-canvas.drawImage(filename,(12 + 4 + 91)*mm,(7 + 2 + 55)*mm)
-canvas.drawImage(filename,(12 + 0)*mm,(7 + 2 + 55)*mm)
-canvas.drawImage(filename,(12 + 4 + 91)*mm,(7 + 0)*mm)
+# white space
+canvas.setFillColor(my_white)
+
+# center white
+canvas.rect(5.7 *mm, (card_height - 45.7)*mm, 79.7 *mm, 42.5 *mm,fill=True,stroke=False)
+# right rect
+canvas.rect(85.4 *mm, (card_height - 43.2)*mm, 2.4 *mm, 37.5 *mm,fill=True,stroke=False )
+# left rect
+canvas.rect(3.2 *mm, (card_height - 43.2)*mm, 2.5 *mm, 37.5 *mm,fill=True,stroke=False)
+
+# top right
+canvas.circle(85.3 *mm, (card_height - 5.7) *mm , r *mm, fill=True,stroke=False)
+# top left
+canvas.circle(5.7 *mm, (card_height - 5.7) *mm , r *mm, fill=True,stroke=False)
+# down rignt
+canvas.circle(85.3 *mm, (card_height - 43.2) *mm , r *mm, fill=True,stroke=False)
+# down left
+canvas.circle(5.7 *mm, (card_height - 43.2) *mm , r *mm, fill=True,stroke=False)
+
+
 
 canvas.save()
 print('done...')
--- src/__init__.py
+++ src/__init__.py
@@ -5,6 +5,7 @@ from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.cidfonts import UnicodeCIDFont
 from reportlab.pdfbase.pdfmetrics import registerFont
 from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.lib.colors import Color,blue,white,red,black
 
 from bs4 import BeautifulSoup
 import requests
