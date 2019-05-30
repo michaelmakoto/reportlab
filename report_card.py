95070fb6edd65c61846ed6b30948cf86a466b982
michaelmartinsen <michaelmartinsen@michaelmartinsen-no-Mac-mini.local>
2019-05-30 14:42:15 +0900

    added the setup func to init card and translation

--- report_card.py
+++ report_card.py
@@ -1,30 +1,52 @@
 from src import *
 from src.card import Card
+from src.translate import Translate
+
+#--- set card class for every word----
+def set_up_card(word):
+    # get translation first
+    t = Translate(word)
+    t.get_toeic()
+    t.get_part()
+    t.get_japanese()
+
+    # if the japanese is too long restrict it
+    if (len(t.japanese) >= 16):
+        t.restrict_japanese()
+
+    # set up card class with translation data
+    c = Card(word)
+    c.japanese = t.japanese
+    c.toeic = t.toeic
+    c.part = t.part
+    
+    return c
+
+#--- main ---
+start = time.time()
 
 print('start...')
 canvas = canvas.Canvas("card.pdf")
 pdfmetrics.registerFont(UnicodeCIDFont('HeiseiMin-W3'))
 
 listOfWrods = [
-    'interlocking','duplex',
     'Commission','commercial','irritating','radiation',
     'electromagnetic','cancer','sequence','infer'
+    
 ]
     
-count = 0
-final_count = len(listOfWrods)
+index = 0
+final_index = len(listOfWrods)
+cards = [set_up_card(l) for l in listOfWrods]
 
-while (count < final_count):
+print('\nstart printing...')
+while (index < final_index):
     for x,y in itertools.product(range(2),range(5)):
         try:
-            word = listOfWrods[count].rstrip().lower()
-            print(count,word)
-
-            card = Card(word)
-            card.set_backgroud(canvas,x,y)
-            card.set_main_frame(canvas,x,y,0)
-
-            count += 1
+            print(index,cards[index].word)
+            cards[index].set_backgroud(canvas,x,y)
+            cards[index].set_main_frame(canvas,x,y,0)
+            index += 1
         except IndexError:
             break
         except:
@@ -34,4 +56,7 @@ while (count < final_count):
 
 canvas.save()
 print('done...')
+end = time.time()
+print('time: {}[sec]'.format(end - start))
+
 os.system('open card.pdf')
